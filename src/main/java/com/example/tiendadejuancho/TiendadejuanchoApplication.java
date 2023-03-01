9f25117ece89de8e4bc6c821e7472950cba3d02a
MILLERxJ <contactomillerxj@gmail.com>
2023-02-28 20:38:24 -0500

    Se crea conexion a BD

---
+++ src/main/java/com/example/tiendadejuancho/TiendadejuanchoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiendadejuancho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendadejuanchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendadejuanchoApplication.class, args);
+	}
+
+}
